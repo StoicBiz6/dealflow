e49054b05efdfbd1608a7ef6aece40aaba387839
Marius <mb@stoicpartner.com>
2026-04-02 20:38:00 -0400

    Fix App.jsx - remove duplicate BrowserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-﻿import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+﻿import { Routes, Route, Navigate } from 'react-router-dom'
 import { SignedIn, SignedOut, RedirectToSignIn } from '@clerk/clerk-react'
 import ModeSelectorPage from './pages/ModeSelectorPage'
 import SellShell from './components/sell-side/SellShell'
@@ -11,12 +11,10 @@ import LandingPage from './LandingPage'
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/select" element={<><SignedIn><ModeSelectorPage /></SignedIn><SignedOut><RedirectToSignIn /></SignedOut></>} />
-        <Route path="/sell/*" element={<><SignedIn><SellShell><Routes><Route path="/" element={<Navigate to="dashboard" replace />} /><Route path="dashboard" element={<SSDashboard />} /><Route path="processes" element={<SSProcesses />} /><Route path="buyers" element={<SSBuyers />} /><Route path="bids" element={<SSBids />} /><Route path="dataroom" element={<SSDataRoom />} /></Routes></SellShell></SignedIn><SignedOut><RedirectToSignIn /></SignedOut></> } />
-        <Route path="/*" element={<LandingPage />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/select" element={<><SignedIn><ModeSelectorPage /></SignedIn><SignedOut><RedirectToSignIn /></SignedOut></>} />
+      <Route path="/sell/*" element={<><SignedIn><SellShell><Routes><Route path="/" element={<Navigate to="dashboard" replace />} /><Route path="dashboard" element={<SSDashboard />} /><Route path="processes" element={<SSProcesses />} /><Route path="buyers" element={<SSBuyers />} /><Route path="bids" element={<SSBids />} /><Route path="dataroom" element={<SSDataRoom />} /></Routes></SellShell></SignedIn><SignedOut><RedirectToSignIn /></SignedOut></>} />
+      <Route path="/*" element={<LandingPage />} />
+    </Routes>
   )
 }
